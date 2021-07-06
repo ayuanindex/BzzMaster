02df8af23a3f3ea0238f7f9aeb5ae27389d4417f
AYuan <lzy2118053626@hotmail.com>
2021-07-06 15:03:02 +0800

    HomeBannerViewHolder广告栏轮播

--- app/src/main/java/com/shenkong/bzzmaster/ui/fragment/home/viewholder/HomeBannerViewHolder.java
+++ app/src/main/java/com/shenkong/bzzmaster/ui/fragment/home/viewholder/HomeBannerViewHolder.java
@@ -5,6 +5,8 @@ import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
@@ -12,11 +14,13 @@ import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 import com.shenkong.bzzmaster.R;
 import com.shenkong.bzzmaster.model.bean.BannerBean;
-import com.shenkong.bzzmaster.ui.fragment.home.adapter.MultipleAdapter;
 import com.shenkong.bzzmaster.ui.fragment.home.HomeBannerFragment;
+import com.shenkong.bzzmaster.ui.fragment.home.adapter.MultipleAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class HomeBannerViewHolder extends MultipleAdapter.MultipleBaseViewHolder {
     public View rootView;
@@ -24,12 +28,15 @@ public class HomeBannerViewHolder extends MultipleAdapter.MultipleBaseViewHolder
     public TabLayout tabLayout;
     private MultipleAdapter multipleAdapter;
     private BannerBean bannerBean;
+    private static final MutableLiveData<Integer> itemPosition = new MutableLiveData<>(0);
+    private static Timer bannerTimer;
+    private static TimerTask bannerTimerTask;
 
     public HomeBannerViewHolder(View rootView) {
         super(rootView);
         this.rootView = rootView;
-        this.bannerPager = (ViewPager2) rootView.findViewById(R.id.bannerPager);
-        this.tabLayout = (TabLayout) rootView.findViewById(R.id.tabLayout);
+        this.bannerPager = rootView.findViewById(R.id.bannerPager);
+        this.tabLayout = rootView.findViewById(R.id.tabLayout);
     }
 
     @Override
@@ -49,12 +56,56 @@ public class HomeBannerViewHolder extends MultipleAdapter.MultipleBaseViewHolder
         new TabLayoutMediator(tabLayout, bannerPager, new TabLayoutMediator.TabConfigurationStrategy() {
             @Override
             public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
-
             }
         }).attach();
+
+        // 订阅数据变化
+        itemPosition.observe(multipleAdapter.getFragmentActivity(), new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer item) {
+                bannerPager.setCurrentItem(item);
+            }
+        });
+
+        startBannerLooper(bannerPagerAdapter.getItemCount());
+    }
+
+
+    /**
+     * 开启广告循环切换定时器
+     *
+     * @param itemCount 广告总条数
+     */
+    private void startBannerLooper(int itemCount) {
+        if (bannerTimer != null) {
+            bannerTimer.cancel();
+            bannerTimer = null;
+        }
+
+        if (bannerTimerTask != null) {
+            bannerTimerTask.cancel();
+            bannerTimerTask = null;
+        }
+
+        bannerTimer = new Timer();
+        bannerTimerTask = new TimerTask() {
+            @Override
+            public void run() {
+                Integer value = itemPosition.getValue();
+                value += 1;
+
+                if (value > itemCount) {
+                    itemPosition.postValue(0);
+                } else {
+                    itemPosition.postValue(value);
+                }
+            }
+        };
+        bannerTimer.schedule(bannerTimerTask, 5000, 5000);
     }
 
     static class BannerPagerAdapter extends FragmentStateAdapter {
+
         private final List<Fragment> fragments;
 
         public BannerPagerAdapter(@NonNull FragmentActivity fragmentActivity, List<Fragment> fragments) {
@@ -72,6 +123,6 @@ public class HomeBannerViewHolder extends MultipleAdapter.MultipleBaseViewHolder
         public int getItemCount() {
             return fragments.size();
         }
-    }
 
+    }
 }
