9b6cd112d17f88f7102570d07209591147803837
AYuan <lzy2118053626@hotmail.com>
2021-08-09 23:39:22 +0800

    添加Beta版渠道

--- app/src/main/java/com/shenkong/bzzmaster/common/utils/download/AppFileProvider.java
+++ app/src/main/java/com/shenkong/bzzmaster/common/utils/download/AppFileProvider.java
@@ -2,15 +2,19 @@ package com.shenkong.bzzmaster.common.utils.download;
 
 import android.content.Context;
 import android.net.Uri;
+
 import androidx.core.content.FileProvider;
+
+import com.shenkong.bzzmaster.BuildConfig;
 
 import java.io.File;
 
 public class AppFileProvider extends FileProvider {
 
-    public static final String AUTHORITY = "com.shenkong.bzzmaster.fileprovider";
+    //public static final String AUTHORITY = "com.shenkong.bzzmaster.fileprovider";
+    public static final String AUTHORITY = BuildConfig.APPLICATION_ID + ".fileprovider";
 
-    public static Uri getUriForFile(Context context, File file){
+    public static Uri getUriForFile(Context context, File file) {
         return FileProvider.getUriForFile(context, AUTHORITY, file);
     }
 }
